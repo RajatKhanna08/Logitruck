f0e5a610ab84d4dabf3201f7b6c5ae3ec6e43dc0
harshitxrajput <harshitxrajput@gmail.com>
2025-07-12 12:59:18 +0530

    starting other pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HomePage from './pages/HomePage'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import IntroLoader from './components/IntroLoader';
 
-const App = () => {
-    return (
-        <div className='relative w-full h-full'>
-            <BrowserRouter>
-                <Navbar />
-                <Routes>
-                    <Route path={"/"} element={<HomePage />} />
-                </Routes>
-                
-            </BrowserRouter>
+const AppWrapper = () => {
+    const location = useLocation();
+    const isHome = location.pathname === "/";
+    const [showIntro, setShowIntro] = useState(false);
 
-            {/* <IntroLoader /> */}
+    useEffect(() => {
+        const hasSeenIntro = sessionStorage.getItem("homeIntroSeen");
+        if(!hasSeenIntro && isHome){
+            setShowIntro(true);
+        }
+    }, [isHome]);
+
+    const handleIntroFinish = () => {
+        sessionStorage.setItem("homeIntroSeen", "true");
+        setShowIntro(false);
+    }
+    return (
+         <div className={`relative w-full h-full ${showIntro ? 'overflow-hidden' : ''}`}>
+      <div className="relative z-0">
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/service' element={<></>} />
+        </Routes>
+      </div>
+
+      {/* Loader sits above everything */}
+      {isHome && showIntro && (
+        <div className="absolute top-0 left-0 w-full h-full z-50 pointer-events-none">
+          <IntroLoader onFinish={handleIntroFinish} />
         </div>
+      )}
+    </div>
+    )
+}
+
+const App = () => {
+
+    return (
+        <BrowserRouter>    
+            <AppWrapper />
+        </BrowserRouter>
   )
 }
 
