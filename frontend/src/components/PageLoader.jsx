49432dec1bcf35a7c07c9b0f799d4470307d1fcb
RajatKhanna08 <rajatkhanna1108@gmail.com>
2025-08-04 21:06:57 +0530

    comment

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -5,7 +5,7 @@ const PageLoader = () => {
         <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/60">
             <div className="bg-gradient-to-r from-yellow-300 via-blue-100 to-[#192a67] px-3 rounded-2xl shadow-md shadow-black flex items-center justify-center">
                 <img
-                    src="/TruckLoader5.gif" // Replace with your animated image (e.g. a moving truck)
+                    src="/TruckLoader5.gif" 
                     alt="Loading..."
                     className="size-30"
                 />
